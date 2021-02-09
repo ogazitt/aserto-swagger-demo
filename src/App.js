67ec76389132c01d9be18463fbc8ee5b968b7d1c
Omri Gazitt <ogazitt@gmail.com>
2021-02-08 19:07:56 -0800

    replaced App with aserto-swagger-ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import AsertoSwaggerUI from '@aserto/aserto-swagger-ui'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AsertoSwaggerUI 
+        style={{ margin: 10 }} 
+        url='https://onebox.demo.aserto.com/openapi/swagger.json' 
+      />
     </div>
   );
 }
